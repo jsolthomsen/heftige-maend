a389ba6482a5d824ca3eb0e6b91ab2d5404fdfa5
NicklasEAA <eaa23nrfa@students.eaaa.dk>
2023-11-17 10:58:32 +0100

    lavet scroll til visu

---
+++ 1semester-projekt/main.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("button1").addEventListener("click", function() {
+        // Find det element, du vil rulle til
+        var targetElement = document.getElementById("div1");
+
+        // Rul til elementet
+        targetElement.scrollIntoView({ behavior: "smooth", block: "end"});
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("button2").addEventListener("click", function() {
+        // Find det element, du vil rulle til
+        var targetElement = document.getElementById("div2");
+
+        // Rul til elementet
+        targetElement.scrollIntoView({ behavior: "smooth", block: "end" });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("button3").addEventListener("click", function() {
+        // Find det element, du vil rulle til
+        var targetElement = document.getElementById("div3");
+
+        // Rul til elementet
+        targetElement.scrollIntoView({ behavior: "smooth", block: "end" });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("button4").addEventListener("click", function() {
+        // Find det element, du vil rulle til
+        var targetElement = document.getElementById("div4");
+
+        // Rul til elementet
+        targetElement.scrollIntoView({ behavior: "smooth", block: "end" });
+    });
+});
--- main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("button1").addEventListener("click", function() {
-        // Find det element, du vil rulle til
-        var targetElement = document.getElementById("div4");
-
-        // Rul til elementet
-        targetElement.scrollIntoView({ behavior: "smooth" });
-    });
-});
