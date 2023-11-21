d3423dd02afac655293d056bcb2dfcf6d5381108
jsolthomsen <jsol.thomsen@gmail.com>
2023-11-21 08:43:29 +0100

    update visualisering_2

--- 1semester-projekt/visualisering_2/visualisering_2.js
+++ 1semester-projekt/visualisering_2/visualisering_2.js
@@ -23,6 +23,47 @@ d3.json("http://localhost:3000/activities-fatal")
     // List of activities to group by
     let activities = ["Surfing", "Swimming", "Spearfishing", "Bathing", "Wading", "Diving", "Standing", "Snorkeling", "Scuba Diving", "Body Boarding",];
     
+    var x = d3.scaleLinear().range([0, width]);
+    var y = d3.scaleLinear().range([0, height]);
+
+    function resetZoom() {
+        const transition = svg.transition().duration(750);
+        x.domain([0, width]);
+        y.domain([0, height]);
+    
+        svg.selectAll("rect")
+            .transition(transition)
+            .attr("x", (d) => x(d.x0))
+            .attr("y", (d) => y(d.y0))
+            .attr("width", (d) => x(d.x1) - x(d.x0))
+            .attr("height", (d) => y(d.y1) - y(d.y0));
+    
+        svg.selectAll("text")
+            .transition(transition)
+            .style("display", "block")
+            .attr("x", (d) => x(d.x0) + 5)
+            .attr("y", (d) => y(d.y0) + 20);
+    }
+
+    function zoom (event, d) {
+        const transition = svg.transition().duration(750);
+       
+        x.domain([d.x0, d.x1]);
+        y.domain([d.y0, d.y1]);
+    
+        svg.selectAll("text").style("display", "none");
+
+        svg.selectAll("rect")
+            .transition(transition)
+            .attr("x", (d) => x(d.x0))
+            .attr("y", (d) => y(d.y0))
+            .attr("width", (d) => x(d.x1) - x(d.x0))
+            .attr("height", (d) => y(d.y1) - y(d.y0));
+           
+    }
+
+    svg.on("dblclick", resetZoom);
+
     let groupedData = groupActivities(data, activities);
   
     var root = d3.hierarchy(groupedData).sum(function(d){ return d.value})
@@ -43,6 +84,7 @@ d3.json("http://localhost:3000/activities-fatal")
       .attr('height', function (d) { return d.y1 - d.y0; })
       .style("stroke", "black")
       .style("fill", "darkred")
+      .on("click", zoom)
      
       svg
       .selectAll("text")
@@ -54,44 +96,27 @@ d3.json("http://localhost:3000/activities-fatal")
         .text(function(d){ return d.data.name })
         .attr("font-size", "20px")
         .attr("fill", "white")
-      
   }
-  
+
   function groupActivities(data, activities) {
     let grouped = {};
-  
+
     data.forEach(d => {
-      // Check if the activity matches any of the specified activities
-      activities.forEach(activity => {
-        if (d.activity.toLowerCase().includes(activity.toLowerCase())) {
-          if (!grouped[activity]) {
-            grouped[activity] = { Yes: 0, No: 0 };
-          }
-  
-          // Count Yes and No
-          if (d.fatal === "Y") {
-            grouped[activity].Yes += 1;
-          } else if (d.fatal === "N") {
-            grouped[activity].No += 1;
-          }
-        }
-      });
+        activities.forEach(activity => {
+            if (d.activity.toLowerCase().includes(activity.toLowerCase())) {
+                if (!grouped[activity]) {
+                    grouped[activity] = 0;
+                }
+                grouped[activity] += 1;
+            }
+        });
     });
-  
-    // Convert grouped data into a format suitable for a D3 treemap
+
     return {
-      name: "Activities",
-      children: Object.entries(grouped).map(([activity, counts]) => ({
-        name: activity,
-        children: Object.entries(counts).map(([fatal, count]) => ({
-          name: fatal,
-          value: count
+        name: "Activities",
+        children: Object.entries(grouped).map(([activity, count]) => ({
+            name: activity,
+            value: count
         }))
-      }))
     };
-  }
-  
-
-
-
-
+}
