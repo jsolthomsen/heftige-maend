efaeafed7275f27a39dd3e2d7c06c643fece128f
jsolthomsen <jsol.thomsen@gmail.com>
2023-11-21 13:28:57 +0100

    vis_2

--- 1semester-projekt/visualisering_2/visualisering_2.js
+++ 1semester-projekt/visualisering_2/visualisering_2.js
@@ -10,7 +10,7 @@ const svg = d3.select("#my_dataviz")
 .append("g")
   .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
-        
+
 // Loader JSON-data fra localhost, og starter vores treemap funktion.
 d3.json("http://localhost:3000/activities-fatal")
   .then(function(data) {
@@ -29,39 +29,81 @@ d3.json("http://localhost:3000/activities-fatal")
 
     // Zoom-in funktion på treemaps enkelte arealer
     function zoom (event, d) {
-      const transition = svg.transition().duration(750);
+      const transition = svg.transition().duration(1000);
+      const detailData = d.parent.children || [];
+      const detailGroups = svg.selectAll(".detail")
+          .data(detailData)
+          .enter()
+          .append("g")
+          .attr("class", "detail");
      
-      x.domain([d.x0, d.x1]);
-      y.domain([d.y0, d.y1]);
+      x.domain([d.parent.x0, d.parent.x1]);
+      y.domain([d.parent.y0, d.parent.y1]);
   
-      svg.selectAll("text").style("display", "none");
-
-      svg.selectAll("rect")
+      svg.selectAll("text")
           .transition(transition)
-          .attr("x", (d) => x(d.x0))
-          .attr("y", (d) => y(d.y0))
-          .attr("width", (d) => x(d.x1) - x(d.x0))
-          .attr("height", (d) => y(d.y1) - y(d.y0));
-         
+          .style("opacity", "0");
+
+          svg.selectAll("rect")
+          .transition(transition)
+          .attr("x", function(d) { return x(d.parent.x0); })
+          .attr("y", function(d) { return y(d.parent.y0); })
+          .attr("width", function(d) { return x(d.parent.x1) - x(d.parent.x0); })
+          .attr("height", function(d) { return y(d.parent.y1) - y(d.parent.y0); });
+      
+         detailGroups.append("rect")
+              .transition()
+              .duration(1000)
+              .attr("x", function(d) { return x(d.x0); })
+              .attr("y", function(d) { return y(d.y0); })
+              .attr("width", function(d) { return x(d.x1) - x(d.x0); })
+              .attr("height", function(d) { return y(d.y1) - y(d.y0); })
+              .style("fill", function(d) { return d.data.name === 'Fatal' ? 'red' : 'green'; });
+          
+          detailGroups.append("text")
+              .transition()
+              .duration(1000)
+              .attr("x", function(d) { if (d.data.name === "Fatal") 
+              { return d.x1 - d.x0 } else 
+              { return d.x1 - d.x0 }
+              })
+              .attr("y", function(d) { return d.y1 - d.y0 })
+              .text(function(d) { return d.data.name})
+              .style("text-anchor", "middle")
+              .attr("font-size", "18px")
+              .attr("fill", "white");
+        
+         detailGroups.append("text")
+              .transition()
+              .duration(1000)
+              .attr("x", function(d) { return x(d.x0); })
+              .attr("y", function(d) { return y(d.y0) + 50; })
+              .text(function(d) { return d.data.value; })
+              .style("text-anchor", "middle")
+              .attr("font-size", "18px")
+              .attr("fill", "white");
   }
 // Zoomer ud når man double-clicker - skal evt. aktiveres anderledes.
     function resetZoom() {
-        const transition = svg.transition().duration(750);
+        const transition = svg.transition().duration(1000);
         x.domain([0, width]);
         y.domain([0, height]);
     
         svg.selectAll("rect")
             .transition(transition)
-            .attr("x", (d) => x(d.x0))
-            .attr("y", (d) => y(d.y0))
-            .attr("width", (d) => x(d.x1) - x(d.x0))
-            .attr("height", (d) => y(d.y1) - y(d.y0));
+            .attr("x", (d) => x(d.parent.x0))
+            .attr("y", (d) => y(d.parent.y0))
+            .attr("width", (d) => x(d.parent.x1) - x(d.parent.x0))
+            .attr("height", (d) => y(d.parent.y1) - y(d.parent.y0));
     
         svg.selectAll("text")
-            .transition(transition)
-            .style("display", "block")
-            .attr("x", (d) => x(d.x0) + 5)
-            .attr("y", (d) => y(d.y0) + 20);
+            .transition()
+            .duration(2000)
+            .style("opacity", "1")
+            .attr("x", (d) => x(d.parent.x0) + 5)
+            .attr("y", (d) => y(d.parent.y0) + 20);
+            
+      svg.selectAll(".detail").transition().duration(500).remove();
     }
 // Deklarer aktivitet dataen som en gruppering - groupActivites defineres længere nede
     let groupedData = groupActivities(data, activities);
@@ -78,25 +120,30 @@ d3.json("http://localhost:3000/activities-fatal")
     .data(root.leaves())
     .enter()
     .append("rect")
-      .attr('x', function (d) { return d.x0; })
-      .attr('y', function (d) { return d.y0; })
-      .attr('width', function (d) { return d.x1 - d.x0; })
-      .attr('height', function (d) { return d.y1 - d.y0; })
+      .attr('x', function (d) { return d.parent.x0; })
+      .attr('y', function (d) { return d.parent.y0; })
+      .attr('width', function (d) { return d.parent.x1 - d.parent.x0; })
+      .attr('height', function (d) { return d.parent.y1 - d.parent.y0; })
       .style("stroke", "black")
       .style("fill", "darkred")
-      .on("click", zoom)
-      .on("dblclick", resetZoom);
+      .on("click", zoom);
+      
   // Sætter tekst på de enkelte arealer
       svg
       .selectAll("text")
       .data(root.leaves())
       .enter()
       .append("text")
-        .attr("x", function(d){ return d.x0+5})
-        .attr("y", function(d){ return d.y0+20}) 
-        .text(function(d){ return d.data.name })
+        .attr("x", function(d){ return d.parent.x0+5})
+        .attr("y", function(d){ return d.parent.y0+20}) 
+        .text(function(d){  
+          console.log(d);
+          return d.parent.data.name 
+        })
         .attr("font-size", "18px")
         .attr("fill", "white")
+
+        svg.on("dblclick", resetZoom)
   }
 
   // Funktion for at sætte gruppering til aktiviter til at være så mange som der er i vores aktiviteter array.
@@ -104,20 +151,27 @@ d3.json("http://localhost:3000/activities-fatal")
   function groupActivities(data, activities) {
     let grouped = {};
     data.forEach(d => {
-        activities.forEach(activity => {
-            if (d.activity.toLowerCase().includes(activity.toLowerCase())) {
-                if (!grouped[activity]) {
-                    grouped[activity] = 0;
-                }
-                grouped[activity] += 1;
-            }
-        });
-    });
-    return {
-        name: "Activities",
-        children: Object.entries(grouped).map(([activity, count]) => ({
-            name: activity,
+      activities.forEach(activity => {
+          if (d.activity.toLowerCase().includes(activity.toLowerCase())) {
+              if (!grouped[activity]) {
+                  grouped[activity] = { 'Fatal': 0, 'Non-Fatal': 0 };
+              }
+              if (d.fatal === 'Y') {
+                  grouped[activity]['Fatal'] += 1;
+              } else {
+                  grouped[activity]['Non-Fatal'] += 1;
+              }
+          }
+      });
+  });
+  return {
+    name: "Activities",
+    children: Object.entries(grouped).map(([activity, counts]) => ({
+        name: activity,
+        children: Object.entries(counts).map(([type, count]) => ({
+            name: type,
             value: count
         }))
-    };
+    }))
+};
 }
